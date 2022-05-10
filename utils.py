2158772d81fbfed9fff8c090591e1bc06fcecaf5
Pranshu Bahadur <pranshubahadur@gmail.com>
2022-05-10 15:45:52 +0530

    Dataset Distrbution

--- utils.py
+++ utils.py
@@ -18,12 +18,14 @@ def imc22_train_directory_parser(train_directory : str,
                 read_csv(path.abspath(f'{train_directory}/{x}/{target_file}')),\
                 how='cross'), listdir(train_directory))))
 
-
+#TODO get distributions
 
 class TrainIMC22Dataset(Dataset):
     def __init__(self, train_directory : str):
         self.train_directory = train_directory
         self.file_df = imc22_train_directory_parser(train_directory)
+        
+        self.distn = self.file_df['basename'].value_counts()
 
     def __getitem__(self, idx : int) -> Tuple[Tensor, List[Tensor]]:
         row = self.file_df.iloc[idx]
